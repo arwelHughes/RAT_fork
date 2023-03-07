d4248f8ed02dc9c9960b371b4366f74777e17847
Arwel Hughes <arwel.hughes@stfc.ac.uk>
2023-03-07 09:07:52 +0000

    Removed old customDev folder from examples. Use the one in /common instead

--- examples/custom dev/customMain.m
+++
@@ -1,8 +0,0 @@
-
-% Main for working with custom script....
-params = [3.0000   20.0000    0.2000   55.0000    0.2000    0.1000    4.0000    2.0000];
-bulk_out = [6.35e-6 ; 0 ; 0];
-bulk_in = 2.073e-6;
-contrast = 1;
-
-[output,sub_rough] = customBilayer(params,bulk_in,bulk_out,contrast);

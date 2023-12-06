27c3a8a7b88c86bc80d40ff5c0900094cb58bf15
arwelHughes <arwelHughes@users.noreply.github.com>
2023-12-06 15:04:35 +0000

    Test images in MD

--- examples/sundry/absorption/Custom Layers/compileMain.m
+++
@@ -1,7 +0,0 @@
-% Main for compile of custom model with Matlab coder...
-
-compilePars = load('compilePars');
-cp = compilePars.compilePars;
-
-[out1,out2] = volume_thiolsam_plus_DPPC_polarised_abs(cp{1},cp{2},cp{3},cp{4});
-

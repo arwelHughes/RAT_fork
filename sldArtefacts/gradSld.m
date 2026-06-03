ddc4a85bcc4040e73cdc91970426aef2e97d0c17
arwel <arwel.hughes@stfc.ac.uk>
2026-06-03 07:50:31 +0100

    Sclaing now works of erf SLD test

--- sldArtefacts/gradSld.m
+++ sldArtefacts/gradSld.m
@@ -1,13 +1,13 @@
-%% Stack version
+%% ERF Stack version
 
 z = 0:200;
-bulkIn = 2;
+bulkIn = 2.073;
 bulkOut = 6.3;
 subRough = 3.5;
 
 layers = [20,  3.4,  3;
           20,  -0.5, 6;
-          20,   6,   3;
+          20,   6,   6;
           20,  -0.5, 3;
           15,   4,   3];
 
@@ -20,7 +20,7 @@ stack = [layers; outLayer];
 
 nLayers = size(stack,1);
 allFuncs = zeros(length(z),nLayers);
-alpha = zeros(nLayers,1);
+alpha = zeros(1,nLayers);
 
 lastLayer = bulkIn;
 thisPos = 30;
@@ -33,23 +33,19 @@ for i = 1:nLayers
 
     thisFun = normcdf(z,thisPos,thisRough);
     if diff < 1
-        thisFun = -thisFun + 1;
+        thisFun = -thisFun;
     end
 
     allFuncs(:,i) = thisFun(:);
     alpha(i) = abs(diff);
     thisPos = stack(i,1) + thisPos;
     lastLayer = nextLayer;
+
 end
 
-allFuncs = allFuncs .* alpha';
-total = sum(allFuncs,2); %/sum(alpha);
-
-% Find the correction....
-actualSldSub = total(1);
-difference = actualSldSub - bulkIn;
-total = total - difference;
-total = total * 1e-6;
+allFuncs = allFuncs .* alpha;
+total = sum(allFuncs,2); 
+total = (total + bulkIn) * 1e-6;
 
 figure(3); clf
 plot(z,total);
